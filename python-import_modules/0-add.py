d8dd877d7056a2bd73363d5a57b7d086fbf8acc1
Jessterr <Jessterr@gmail.com>
2024-01-09 15:53:16 -0600

    minor changes to 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import add_0
+
 if __name__ == "__main__":
-    """Print the sum of a and b."""
+    from add_0 import add as _add
+
     a = 1
     b = 2
-    result = add_0.add(a, b)
+
+    result = _add(a, b)
     print("{} + {} = {}".format(a, b, result))
