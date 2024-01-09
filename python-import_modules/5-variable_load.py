7e0ac923350ed625bfd8395a0d5443ed3c7cb85f
Jessterr <Jessterr@gmail.com>
2024-01-09 01:36:57 -0600

    5-variable_load.py

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+    """imports variable a from file variable_load_5.py and prints its value."""
+    from variable_load_5 import a
+
+    print(a)
