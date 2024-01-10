d318fd4469d2c54c227836a5185dc52d83a46d1e
Jessterr <Jessterr@gmail.com>
2024-01-09 21:05:31 -0600

    4-hidden_discovery.py test 2

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import hidden_4
+import importlib
 
 
-def discover_names(module):
+def discover_names(module_name):
+    module = importlib.import_module(module_name)
     for name in dir(module):
         if not name.startswith("__"):
             print(name)
 
 
 if __name__ == "__main__":
-    module = hidden_4
-    discover_names(module)
+    module_name = 'hidden_4'
+    discover_names(module_name)
