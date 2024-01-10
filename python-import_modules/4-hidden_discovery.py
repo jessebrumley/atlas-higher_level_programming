b5139674318a7c341f1b07a0b645ac2ff13c3484
Jessterr <Jessterr@gmail.com>
2024-01-09 21:03:54 -0600

    forgot style

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
+
+
 def discover_names(module):
     for name in dir(module):
         if not name.startswith("__"):
             print(name)
 
+
 if __name__ == "__main__":
     module = hidden_4
     discover_names(module)
-
