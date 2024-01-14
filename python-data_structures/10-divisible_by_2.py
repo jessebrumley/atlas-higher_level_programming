e0ed264f1f0634a2cebd4d3a17c0a660e1fd1ddb
Jessterr <Jessterr@gmail.com>
2024-01-14 04:24:46 -0600

    many error

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,5 @@ def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
         for number in my_list:
-            if number % 2:
-                new_list += number
-    return (new_list)
+            new_list.append(False if number % 2 else True)
+        return new_list
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     new_list = []
     if not my_list:
-        return (None)    
+        return (None)
     if my_list:
         my_list.sort(reverse=True)
         return (my_list[0])
