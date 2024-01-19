10e5e18110658357e7d4ef7f878519e4bcb4488f
Jessterr <Jessterr@gmail.com>
2024-01-18 22:54:26 -0600

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
 class Square:
     """Class representing a square"""
 
     def __init__(self, size):
-        """defining the size of the square as a private instance attribute"""
+        """Initializing a square
+        
+        Args: size(int): The size of the new square. Must be an integer.
+        """
         self.__size = size
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Define a class Square"""
+
 class Square:
     """Class representing a square"""
+    
     def __init__(self, size=0):
         """Initializing a square
 
