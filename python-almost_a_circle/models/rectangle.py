2c9e95152b6850bddb3fce9f23dd7ac8191011a5
Jessterr <Jessterr@gmail.com>
2024-01-30 21:42:46 -0600

    ..

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,29 +21,6 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
-#!/usr/bin/python3
-"""Defines a rectangle class."""
-from models.base import Base
-
-
-class Rectangle(Base):
-    """Represents a new rectangle"""
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of a new rectangle.
-            height (int): the height of a new rectangle.
-            x (int): the x location of new rectangle.
-            y (int): the y location of new rectangle.
-        Raises:
-        """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-        super().__init__(id)
-
     @property
     def width(self):
         """Get the width of the Rectangle."""
