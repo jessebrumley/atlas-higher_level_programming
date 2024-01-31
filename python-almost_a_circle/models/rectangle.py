c267d659e4e4de27737068ed75823a172703aa91
Jessterr <Jessterr@gmail.com>
2024-01-30 21:46:19 -0600

    error handling

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -29,6 +29,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the width of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -39,6 +43,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the height of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -49,6 +57,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the x coordinate of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -59,4 +71,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the y coordinate of the Rectangle."""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
