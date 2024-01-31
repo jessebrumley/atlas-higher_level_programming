54975f15cdb89ee7980e9bfa7dc7b2193633b835
Jessterr <Jessterr@gmail.com>
2024-01-30 21:37:49 -0600

    changed attr names

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,10 +15,10 @@ class Rectangle(Base):
             y (int): the y location of new rectangle.
         Raises:
         """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
