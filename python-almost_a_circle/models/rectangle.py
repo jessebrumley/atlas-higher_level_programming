5f6452e38ca8a6710be5211e4813ba6c18a5e1cf
Jessterr <Jessterr@gmail.com>
2024-01-30 21:55:58 -0600

    changed setters to use instance instead of type

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -29,12 +29,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the width of the Rectangle."""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-
 
     @property
     def height(self):
@@ -44,7 +43,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the height of the Rectangle."""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -58,7 +57,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the x coordinate of the Rectangle."""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -72,7 +71,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the y coordinate of the Rectangle."""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
