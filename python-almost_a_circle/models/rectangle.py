57f07f7734dd9b003ce9b49d41191c5df13e7844
Jessterr <Jessterr@gmail.com>
2024-01-30 21:40:38 -0600

    misunderstood how setters work, updated get/sets

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,22 +21,65 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a new rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of a new rectangle.
+            height (int): the height of a new rectangle.
+            x (int): the x location of new rectangle.
+            y (int): the y location of new rectangle.
+        Raises:
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """Get the width of the Rectangle."""
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle."""
+        self.__width = value
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
+        """Get the height of the Rectangle."""
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the Rectangle."""
+        self.__height = value
 
     @property
     def x(self):
-        """Set/get the x coordinate of the Rectangle."""
+        """Get the x coordinate of the Rectangle."""
         return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate of the Rectangle."""
+        self.__x = value
 
     @property
     def y(self):
-        """Set/get the y coordinate of the Rectangle."""
+        """Get the y coordinate of the Rectangle."""
         return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate of the Rectangle."""
+        self.__y = value
