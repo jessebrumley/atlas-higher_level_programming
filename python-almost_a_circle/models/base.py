9b7aab4e1cc3580d9fcc258dbc2967f2c1845c39
Jessterr <Jessterr@gmail.com>
2024-01-31 22:09:24 -0600

    added json.loads

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -48,3 +48,16 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation from json_string.
+
+        Args:
+            json_string: a string representing a list of dictionaries
+        """
+        if json_string is None or json_string == []:
+            return []
+        else:
+            return json.loads(json_string)
