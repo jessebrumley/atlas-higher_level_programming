be704494a05fa768d3e1d0a066ac71042ae25161
Jessterr <Jessterr@gmail.com>
2024-01-31 22:21:57 -0600

    load from file using open -r and returning a list made from create method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -77,3 +77,19 @@ class Base:
                 new = cls(1)
             new.update(**dictionary)
             return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances from '<cls.__name__>.json'
+
+        Returns:
+            If the file doesn’t exist, return an empty list
+            Otherwise, return a list of instances
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
