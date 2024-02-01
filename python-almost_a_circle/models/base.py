41ed0855c79f8f6b5ce7859c00548cae0f15fd44
Jessterr <Jessterr@gmail.com>
2024-01-31 20:11:31 -0600

    added static method that converts list of dictionaries to a json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 """Defines a base class """
+import json
 
 
 class Base:
     """
+    This represents a base template for all classes in this project.
+
     Private Class Attributes:
         __nb_object (int): Number of Bases.
     """
@@ -17,3 +20,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
