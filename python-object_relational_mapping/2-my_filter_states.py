97c7ea6fc15e0a30f3c85cc26a7bda99adb83575
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-02-21 23:25:47 -0600

    test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-
-""" lists all states from the database hbtn_0e_usa """
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-
-    usrn = sys.argv[1]
-    pswd = sys.argv[2]
-    dtbs = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", user=usrn, passwd=pswd, db=dtbs)
-
-    cursor = db.cursor()
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
-
-    for row in cursor.fetchall():
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    cursor.close()
+    cur.close()
     db.close()
