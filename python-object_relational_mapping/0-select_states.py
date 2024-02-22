a361e908da47b7198025ab4f3d7e8e780d7bea7e
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-02-21 23:06:52 -0600

    reverting

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-""" I am unsure why it has no permissions, when I did give u+x.. """
-import MySQLdb
+""" lists all states from the database hbtn_0e_usa """
 import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    try:
-        with MySQLdb.connect(host="localhost", user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3], port=3306) as db:
-            with db.cursor() as cur:
-                cur.execute("SELECT * FROM states")
-                rows = cur.fetchall()
-                for row in rows:
-                    print(row)
-    except MySQLdb.Error as e:
-        print(f"Error {e.args[0]}: {e.args[1]}")
-        sys.exit(1)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port= 3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
