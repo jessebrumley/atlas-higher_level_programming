6530132b3c42d8c085bc493acf7de65058d6c507
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 18:58:43 -0500

    single quote and semicolon

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,4 @@
 /*
 A script that prints "C is fun", "Python is cool", and "Javascript is amazing"
 */
-console.log("C is fun\nPython is cool\nJavaScript is amazing")
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
