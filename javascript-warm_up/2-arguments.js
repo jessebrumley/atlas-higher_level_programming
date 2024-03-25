55a20e4e75977dcabf16357b1d1186bd711eca3b
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:00:29 -0500

    created else/if for arg length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,3 +2,10 @@
 /*
 A script that prints a message depending of the number of arguments passed
 */
+if (process.argv.length <= 2) {
+  console.log('No arguement');
+} else if (process.argv.length === 3) {
+  console.log('Arguement found');
+} else {
+  console.log('Arguements found');
+}
