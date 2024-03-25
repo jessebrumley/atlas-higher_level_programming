ec078f477ac7ee64dec30adf2c99a70e821e8307
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 00:47:50 -0500

    c is fun c is fun c is fun but why c? why not js is fun? bc js is a lot more fun than C...

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,3 +2,14 @@
 /*
 A script that prints x times “C is fun”
 */
+const x = process.argv[2];
+
+const parsedX = parseInt(x, 10);
+
+if (isNaN(parsedX)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parsedX; i++) {
+    console.log('C is fun');
+  }
+}
