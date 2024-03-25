60b7d49f953ae73c131f670f1f0acde23d83d895
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 02:00:06 -0500

    created a factorial loop by taking input from argv, parsing as an int, and adding all numbers up to input.

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,3 +2,12 @@
 /*
 A script that computes and prints a factorial
 */
+function factorial (a) {
+  let result = parseInt(0);
+  for (let i = 0; i <= a; i++) {
+    result += i;
+  }
+  console.log(result);
+}
+
+factorial(Number(process.argv[2]));
