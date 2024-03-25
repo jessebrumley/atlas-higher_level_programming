8956416e8d6f946d22676fac6b985dfee8380be2
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 02:22:16 -0500

    script takes array and ints, sorts in reverse, and prints 2nd position

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,3 +2,10 @@
 /*
 A script that searches the second biggest integer in the list of arguments
 */
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const numberArray = process.argv.slice(2).map(Number);
+  const secondBiggest = numberArray.sort(function (a, b) { return b - a; })[1];
+  console.log(secondBiggest);
+}
