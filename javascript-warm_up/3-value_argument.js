b087ebfa62a13695a54f36c3402916204c45a0c4
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:27:29 -0500

    changed to use try/catch and for...of loops to avoid using length

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,10 +2,12 @@
 /*
 A script that prints the first argument passed to it
 */
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
-  for (let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i]);
+const args = process.argv.slice(2);
+
+try {
+  for (const arg of args) {
+    console.log(arg);
   }
+} catch (error) {
+  console.log('No arguement');
 }
