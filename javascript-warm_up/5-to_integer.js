bebe566a09e592613ee07f19173b5b88ad05b626
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:57:24 -0500

    used isNaN to catch error without using try/catch

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,3 +2,12 @@
 /*
 A script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
 */
+const number = process.argv[2];
+
+const parsedNumber = parseInt(number, 10);
+
+if (isNaN(parsedNumber)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parsedNumber);
+}
