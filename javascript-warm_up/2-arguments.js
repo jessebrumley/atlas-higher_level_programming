2ee138690a7316eee6a09bcdcb607e8528aaabf8
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:02:08 -0500

    fixed typos

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 A script that prints a message depending of the number of arguments passed
 */
 if (process.argv.length <= 2) {
-  console.log('No arguement');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Arguement found');
+  console.log('Argument found');
 } else {
-  console.log('Arguements found');
+  console.log('Arguments found');
 }
