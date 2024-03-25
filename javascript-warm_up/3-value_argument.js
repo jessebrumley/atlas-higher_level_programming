446575d76faf63aa8d513d862788305583357cad
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:32:41 -0500

    try/catch did not work (I forgot to test it). changed to use a simpler version of if/else

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,12 +2,12 @@
 /*
 A script that prints the first argument passed to it
 */
-const args = process.argv.slice(2);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  const args = process.argv.slice(2);
 
-try {
   for (const arg of args) {
     console.log(arg);
   }
-} catch (error) {
-  console.log('No arguement');
 }
