70fe2ec46f07e26c090f959fd208d8d2caf54dcd
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 01:01:34 -0500

    square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,3 +2,19 @@
 /*
 A script that prints a square
 */
+const input = process.argv[2];
+
+const squareSize = parseInt(input, 10);
+
+if (isNaN(squareSize)) {
+  console.log('Missing size');
+} else {
+  let output = '';
+  for (let col = 0; col < squareSize; col++) {
+    for (let row = 0; row < squareSize; row++) {
+      output += 'X';
+    }
+    output += '\n';
+  }
+  console.log(output);
+}
