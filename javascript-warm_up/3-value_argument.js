053f5d801dd22109c2710381a4cbc2195126ff40
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-24 20:11:00 -0500

    added for loop to print each passed arguement

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,3 +2,10 @@
 /*
 A script that prints the first argument passed to it
 */
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+  }
+}
