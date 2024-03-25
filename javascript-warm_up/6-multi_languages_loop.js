57042b1c522924e1389486d92f74574ebfd002a1
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 00:34:17 -0500

    multilang loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,3 +2,8 @@
 /*
 A script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 */
+const multilang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const lang of multilang) {
+  console.log(lang);
+}
