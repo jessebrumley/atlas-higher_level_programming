4d40d2d80d5713e4a1153774e2877ed4a283bf84
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-25 01:13:19 -0500

    testing character count on checker after moving the final row outside of the initial loop and into its own (without the newline)

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,18 +3,22 @@
 A script that prints a square
 */
 const input = process.argv[2];
-
 const squareSize = parseInt(input, 10);
-
+let row = 1;
+let col = 1;
+let output = '';
 if (isNaN(squareSize)) {
   console.log('Missing size');
-} else {
-  let output = '';
-  for (let col = 0; col < squareSize; col++) {
-    for (let row = 0; row < squareSize; row++) {
+} else if (row != squareSize) {
+  for (row = 1; row < squareSize; row++) {
+    for (col = 1; col <= squareSize; col++) {
       output += 'X';
     }
     output += '\n';
   }
-  console.log(output);
+} else {
+  for (let colFinal = 1; colFinal <= squareSize; colFinal++) {
+    output += 'X';
+  }
 }
+console.log(output);
