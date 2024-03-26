ed3f89e0eba740799f9139108d4da83a72f901fe
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 16:32:24 -0500

    used a loop to print the rectangle

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -3,6 +3,25 @@
 An instance method that prints the Rectangle defined in task 2
 */
 class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let output = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output += 'X';
+      }
+      if (i < (this.height - 1)) {
+        output += '\n';
+      }
+    }
+    console.log(output);
+  }
 }
 
 module.exports = Rectangle;
