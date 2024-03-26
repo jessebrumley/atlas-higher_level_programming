91c8a3d40008301a0b01349d45e2834a09e0c249
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 16:49:22 -0500

    style fix

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -5,9 +5,9 @@ A class Square that defines a square and inherits from Rectangle of 4-rectangle.
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-    constructor (size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 }
 
-module.exports = Square
+module.exports = Square;
