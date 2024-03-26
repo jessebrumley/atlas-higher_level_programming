be93984869884ca7542565a5fef63f382f54a103
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 16:03:44 -0500

    rectangle if +, empty object if 0 or -

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,6 +3,12 @@
 A class Rectangle defined in task one except this creates an empty class if either arguement is 0 or lower
 */
 class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
