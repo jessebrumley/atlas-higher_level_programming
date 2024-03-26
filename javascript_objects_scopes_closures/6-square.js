e3a1fb0f9f2d5b3faea39f88b04c6b2677f59237
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 17:28:19 -0500

    I HAD to add this.size=size to 5.square in order to get 6.square to inherit from 5.square properly

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,3 +2,24 @@
 /*
 A class Square that defines a square and inherits from Square of 5-square.js and an instance method that prints it
 */
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    let output = '';
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        output += c;
+      }
+      if (i < this.size - 1) {
+        output += '\n';
+      }
+    }
+    console.log(output);
+  }
+}
+
+module.exports = Square;
