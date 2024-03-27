d3055fbe724f659c65be8ecbaa495bbccd584d58
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 21:05:38 -0500

    using .push to make the reversed list

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,3 +2,10 @@
 /*
 A function that returns the reversed version of a list
 */
+exports.esrever = function (list) {
+  let reverseList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseList.push(list[i]);
+  }
+  return (reverseList);
+};
