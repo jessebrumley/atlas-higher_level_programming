a036963035e23841b727c4e2a9146342eb3b6cb5
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 20:20:00 -0500

    created loop to count occurences of an element in a list

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,3 +2,12 @@
 /*
 A function that returns the number of occurrences in a list
 */
+exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for (let i = 0; i < list.length + 1; i++) {
+        if (list[i] == searchElement) {
+            count++;
+        }
+    }
+    return(count);
+}
