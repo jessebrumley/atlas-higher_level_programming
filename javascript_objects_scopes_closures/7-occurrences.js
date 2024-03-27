4b72e3ad455cf92829c341b0bdc687a29828624b
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 20:22:22 -0500

    style fix

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -3,11 +3,11 @@
 A function that returns the number of occurrences in a list
 */
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    for (let i = 0; i < list.length + 1; i++) {
-        if (list[i] === searchElement) {
-            count++;
-        }
+  let count = 0;
+  for (let i = 0; i < list.length + 1; i++) {
+    if (list[i] === searchElement) {
+      count++;
     }
-    return(count);
-}
+  }
+  return (count);
+};
