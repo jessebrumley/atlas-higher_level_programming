62c24c353aaf9077daef36b7c5748ab6100a3f46
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 21:18:15 -0500

    logging function input while tracking count

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -2,3 +2,8 @@
 /*
 A function that prints the number of arguments already printed and the new argument value.
 */
+let count = 0;
+exports.logMe = function (item) {
+  console.log(count + ': ' + item);
+  count++;
+};
