e429b1dffd62fa370f7521596d1e21c651c93760
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-26 21:31:46 -0500

    I didnt know functions could return another function

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,3 +2,8 @@
 /*
 A function that converts a number from base 10 to another base passed as argument
 */
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
