e944510e2be37d01b9f4cb58bca105b13d23e907
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-04-02 17:17:04 -0500

    used fs.writeFile to create file and report errors

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 /* A script that writes a string to a file. */
+const fs = require('fs');
+
+const file = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(file, data, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
