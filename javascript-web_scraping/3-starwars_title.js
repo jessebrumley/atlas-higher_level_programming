0c2d0ae37be00d2d4a8b22408de7522877d234a8
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-04-02 19:54:02 -0500

    scraping API to print movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 /* A script prints the title of a Star Wars movie where the episode number matches a given integer. */
+const request = require('request');
+const episode = process.argv[2];
+const API = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API + episode, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
