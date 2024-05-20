34e2dc7edb077034ac7814346e81e287ddc5ed52
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-20 16:25:16 -0500

    PEP8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """checks if two classes are the same"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
       return False
