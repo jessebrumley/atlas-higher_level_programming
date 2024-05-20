d554f8073a15348c9eebd9fb251bb8ead650d49c
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-20 17:18:03 -0500

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class BaseGeometry:
+    """Represent a class"""
+
+    def area(self):
+        """defines area of BaseGeometry class"""
+        raise Exception("area() is not implemented")
