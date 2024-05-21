257735b6c401c78a4e3541673b555300046052aa
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 16:05:49 -0500

    test 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+def read_file(filename=""):
+    """
+    Reads a text file in UTF-8 encoding and prints its content to stdout.
+
+    Parameters:
+    filename (str): The name of the file to read. Defaults to an empty string.
+
+    Returns:
+    None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        
+        print(content)
+
+if __name__ == "__main__":
+    filename = "example.txt"
+    read_file(filename)
