3b78236ecbc1ca63e47c4db2f48ac3e36be05934
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 16:53:42 -0500

    test 1-b

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,13 @@
 def write_file(filename="", text=""):
     """
     Writes a given text to a file and returns the number of characters written
+
+    Parameters:
+    filename (str): The name of the file to read or create
+    text (str): The text to add
+
+    Returns:
+    None
     """
     if not filename:
         filename = "output.txt"
