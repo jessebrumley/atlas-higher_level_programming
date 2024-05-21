4fba8430d1859cabc44d9358f4e42987e5750623
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 18:51:55 -0500

    test 3-b

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Converts a Python object to a JSON string"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Converts a Python object to a JSON string
+
+    Parameters:
+    my_obj The Python object to convert to JSON
+
+    Returns:
+    The JSON representation of the object
+    """
+    return json.dumps(my_obj)
+
+
+if __name__ == "__main__":
+    to_json_string()
