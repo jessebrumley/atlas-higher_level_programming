48093b8e023332c274d327459dadcaa81697b729
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 19:30:43 -0500

    6-a

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Creates an Object from a “JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an Object from a “JSON file
+
+    Parameters:
+    filename (str): The name of the file to convert to python object
+
+    Returns:
+    A python object
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+
+if __name__ == "__main__":
+    filename = "output.json"
+    load_from_json_file(filename)
