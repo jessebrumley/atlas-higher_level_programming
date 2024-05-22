43f3cea87666565449fff97ce69bab311fa19299
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 19:44:42 -0500

    test 7-a

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+from os.path import isfile
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+def main():
+    """
+    adds all arguments to a Python list and then saves them to a file
+    """
+    if isfile('./add_item.json'):
+        obj_list = load('./add_item.json')
+    else:
+      obj_list = []
+
+    for arg in sys.argv[1:]:
+        obj_list.append(arg)
+
+    save(obj_list, 'add_item.json')
