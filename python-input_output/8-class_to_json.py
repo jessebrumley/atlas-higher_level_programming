0f95b43e37f9756ed9dcf5db094df7b28bb3a53b
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 20:30:41 -0500

    8-d test

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -14,24 +14,7 @@ def class_to_json(obj):
     Returns:
     A dictionary with a simple data structure
     """
-    json_obj = []
-    
-    for attr_name, attr_value in obj.__dict__.items():
-        if isinstance(attr_value, (list, dict)):
-            json_attr = class_to_json(attr_value)
-        elif isinstance(attr_value, bool):
-            json_attr = str(attr_value).lower()
-        else:
-            json_attr = attr_value
-        
-        json_obj.append({attr_name: json_attr})
-    
-    json_str = "{\n"
-    for item in json_obj:
-        json_str += "  \"" + next(iter(item)) + "\": " + str(item[next(iter(item))]) + ",\n"
-    json_str = json_str.rstrip(",\n") + "\n}\n"
-    
-    return json_str
+    return vars(obj)
 
 
 if __name__ == "__main__":
