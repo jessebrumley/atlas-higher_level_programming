ec072a6797820b00bff0d4cdad5b4ebf22bbe804
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 20:34:29 -0500

    9-a

---
+++ python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+class to define Student attributes
+"""
+
+
+class Student:
+    """
+    Student class
+    
+    to_json: creates a json-like dictionary
+
+    Args:
+        first_name: student's first name
+        last_name: Student's last name
+        age: Student's age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return vars(self)
