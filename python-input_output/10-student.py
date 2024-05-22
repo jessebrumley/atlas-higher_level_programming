9964a0ad0354d186f1df9a7015ba1a046db409e0
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 20:38:25 -0500

    10-a

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+defines class of students
+"""
+
+
+class Student:
+    """
+    Student class
+
+    to_json: creates a json-like dictionary
+
+    Args:
+        first_name: student's first name
+        last_name: Student's last name
+        age: Student's age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dict = vars(self)
+        if attrs is not None:
+            new_dict = dict()
+            for key in attrs:
+                if key in dict:
+                    new_dict[key] = dict[key]
+            return new_dict
+        return dict
