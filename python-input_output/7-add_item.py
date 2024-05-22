c54d472ad3167ccbd4174aebaf113a37f9d1e35b
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-21 19:48:04 -0500

    7-c style changes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,19 +6,20 @@ from os.path import isfile
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
+
 def main():
     """
     adds all arguments to a Python list and then saves them to a file
-    
+
     Parameters:
     All command line arguements after the filename
-    
+
     Return: None
     """
     if isfile('./add_item.json'):
         obj_list = load('./add_item.json')
     else:
-      obj_list = []
+        obj_list = []
 
     for arg in sys.argv[1:]:
         obj_list.append(arg)
