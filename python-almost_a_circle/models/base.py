bab6317e8532e8d8a08e1afbdbd84928514d0b99
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-05-23 12:38:38 -0500

    1b

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,16 +5,16 @@
 Class Base:
     """
     Base Class model used as template for all other classes in this project
-    
+
     Class Attributes:
         __nb_object (int): Number of Bases.
     """
-    
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
-            self.id=id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
