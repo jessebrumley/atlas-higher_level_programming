61d6fbd86c1943530f8ed88e2b220a32e0308d3c
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-27 16:40:39 -0500

    square style fixes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
-  """
-  A subclass Square that inherits from Rectangle
-  """
-  from models.rectangle import Rectangle
-  
-  class Square(Rectangle):
-      """
+"""
+A subclass Square that inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
     A subclass Square that inherits from Rectangle
     """
-    
+
     def __init__(self, size, x=0, y=0, id=None):
-      super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-      id = self.id
-      x = self.x
-      y = self.y
-      size = self.width
-      return "[Square] ({0}) {1}/{2} - {3}".format(id, x, y, size)
+        id = self.id
+        x = self.x
+        y = self.y
+        size = self.width
+        return "[Square] ({0}) {1}/{2} - {3}".format(id, x, y, size)
