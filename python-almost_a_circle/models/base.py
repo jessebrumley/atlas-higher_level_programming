9f147aa99112a718114327568415f6a16b6d3b68
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-27 17:31:27 -0500

    ddddddd

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,3 +41,25 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation from json_string
+        """
+        if json_string is None or json_string == "":
+            return []
+    return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
